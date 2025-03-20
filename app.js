69827af871a23d03515492edfe8cf34f79d917b7
qitao <qitao@web.com>
2025-03-20 14:43:21 +0800

    锚

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import config from "./.migpt.js";
-import { MiGPT } from "./dist/index.cjs";
+import config from './.migpt.js'
+import { MiGPT } from './dist/index.cjs'
 
 async function main() {
-  const client = MiGPT.create(config);
-  await client.start();
+  const client = MiGPT.create(config)
+  console.log(client)
+  await client.start()
 }
 
-main();
+main()
